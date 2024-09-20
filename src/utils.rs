c41737ce8732d9f4828bf1dd537d9b924e94b33a
Karpov Victor <nilafe4@gmail.com>
2024-09-20 15:10:00 +0500

    Init commit

---
+++ src/utils.rs
@@ -0,0 +1,34 @@
+use rand::Rng;
+
+pub fn print_owl() {
+    let owl = r#"
+       ___
+     .~))>>  
+    .~)>>  
+  .~))))>>>                 ___
+.~~))>>             .       (o,o)
+.~~)))>>>)          |      {'   '}  
+.~))>>)>>>          |       -"-"-
+.~))>>))))>>      _____|__________
+~))>>))))>>      '                  `
+~)))))>>)>         |   Owlnigma    |
+  ~))))>>         \________________/
+    "#;
+
+    // ANSI escape codes for colors
+    let colors = [
+        "\x1b[31m", // red
+        "\x1b[32m", // green
+        "\x1b[33m", // yellow
+        "\x1b[34m", // blue
+        "\x1b[35m", // magenta
+        "\x1b[36m", // cyan
+    ];
+    let reset = "\x1b[0m";
+
+    // Генерация случайного индекса для выбора цвета
+    let random_color = colors[rand::thread_rng().gen_range(0..colors.len())];
+
+    // Вывод совы с рандомным цветом
+    println!("{}{}{}", random_color, owl, reset);
+}
